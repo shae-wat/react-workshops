ed5b7d037f983bf795bb3265167b0138b301b9fc
shaelyn-watson <shaelynjoy@gmail.com>
2017-01-09 17:29:53 -0800

    heart animation

--- 2_animationComponent/src/components/Component1.js
+++ 2_animationComponent/src/components/Component1.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import heart from '../images/twitterExplodingHearts.png';
 import classNames from 'classnames';
 
 import './Component1.css';
@@ -8,11 +7,9 @@ class Component1 extends Component {
   render() {
     if (this.props.activeTab !== '1') return null;
 
-    let heart = classNames('Component1__heart', 'Component1__position');
-
     return (
       <div className='Component1__container'>
-        <div className={heart}/>
+        <div className='Component1__heart' />
       </div>
     );
   }
