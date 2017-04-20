db21e15b03414094e1bedfad7b017d0fd9607e50
shae-wat <shaelynjoy@gmail.com>
2017-04-19 18:51:44 -0700

    step, add firebase stuff to gitignore

--- 5_users/src/reducers/user.js
+++ 5_users/src/reducers/user.js
@@ -1,10 +1,17 @@
 const user = (state = {}, action) => {
   switch (action.type) {
+    case '@@redux/INIT':
+      return {
+        id: 1,
+        email: 'shaelynjoy+hardcode@gmail.com',
+        password: 'storedpass',
+      };
+
     case 'ADD_USER':
       return {
         id: action.id,
         email: action.email,
-        password: action.password
+        password: action.password,
       };
 
     default:
